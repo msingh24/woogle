a44baa28c6c2d3c91391b298e8dcf7b6fbeba15b
msingh24 <44597937+msingh24@users.noreply.github.com>
2019-10-10 00:54:35 -0400

    Add files via upload

---
+++ woogle/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Woogle</title>
+<meta name="description" content="Search the web for sites and images">
+  <meta name="keywords" content="Search engine, Woogle, Websites">
+  <meta name="author" content="Maheshwar Singh">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+
+</head>
+
+<body>
+
+<div class="wrapper indexPage">
+
+	<div class="mainSection">
+
+		<div class="logoContainer">
+			<img src="assets/images/Woogle.png">
+		</div>
+
+
+
+        <form action="search.php" method="GET">
+	        <div class="searchContainer">
+				<input class="searchBox" type="text" name="term">
+				<input class="searchButton" type="submit" value="search">
+			</div>
+	    </form>
+
+	</div>
+
+</div>
+
+</body>
+
+</html>
